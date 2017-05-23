2b8e3e61d58cd3a72e2812b8bf278e61ec9bfa5e
Rene Leyva <sinmatonesaporfavor@gmail.com>
2017-05-23 17:00:38 -0500

    Barra de navegación lista

---
+++ js/index.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+	 $('.button-collapse').sideNav();
+
+	 var flag = false; 
+	 $('#search-icon').click(function(e) {
+	 	e.preventDefault();
+	 	if (flag) {
+	 		//lo oculto
+	 		$(this).find('i').text('search');
+	 		$('.search').slideUp('fast');
+	 		flag = false;
+	 	} else {
+	 		//Lo muestro
+	 		$(this).find('i').text('clear')
+		 	$('.search').slideDown('fast');
+		 	$('.search').find('input[type="text"]').focus();
+		 	flag = true;
+	 	}
+	 	
+	 });
+});
