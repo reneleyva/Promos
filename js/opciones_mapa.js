14bbe4a6d78145f54b65c1e957e2ad491e72eb6d
zaide <xhanzary9@gmail.com>
2017-05-29 09:28:58 -0500

    la vista de ver servicios esta lista, solo faltan algunos ajustes en el pop up

--- js/opciones_mapa.js
+++ js/opciones_mapa.js
@@ -222,4 +222,37 @@ function initMap() {
   $(document).ready(function() {
     $('select').material_select();
   });
-            
+  
+  /* -------------------------------- 
+
+xpopup 
+
+-------------------------------- */
+
+jQuery(document).ready(function($){
+  //open popup
+  $('.cd-popup-trigger').on('click', function(event){
+    event.preventDefault();
+    $('.cd-popup').addClass('is-visible');
+    $('body').css('overflow', 'hidden');
+  });
+  
+  //close popup
+  $('.cd-popup').on('click', function(event){
+    if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+      event.preventDefault();
+      $(this).removeClass('is-visible');
+      $('body').css('overflow', 'visible');
+    }
+  });
+  //close popup when clicking the esc keyboard button
+  $(document).keyup(function(event){
+      if(event.which=='27'){
+        $('.cd-popup').removeClass('is-visible');
+        $('body').css('overflow', 'visible');
+      }
+    });
+});
+
+
+
