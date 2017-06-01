71ce6d613f01b3d757bd8a86fb2de03b0b151d92
Rene Leyva <sinmatonesaporfavor@gmail.com>
2017-06-01 14:17:10 -0500

    barra fija

--- js/index.js
+++ js/index.js
@@ -20,5 +20,19 @@ jQuery(document).ready(function($) {
 	 	
 	 });
 
+	 $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      	console.log($(window).scrollTop())
+		if ($(window).scrollTop() > 376) {
+			console.log("Fixed");
+		  $('#tabs').addClass('fixed');
+		}
+		if ($(window).scrollTop() < 377) {
+		  $('#tabs').removeClass('fixed');
+		}
+		});
+
 	 
 });
