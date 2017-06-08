4c1eb5ae7fcf8575a999a73c624d28619b7c2adb
Rene Leyva <sinmatonesaporfavor@gmail.com>
2017-06-08 00:38:16 -0500

    neon

---
+++ js/registrarse-controlador.js
@@ -0,0 +1,90 @@
+/* Controlador para verPrducto
+ * Agrega los datos necesarios a las vistas. 
+ */
+
+
+ /* Función IFFE para el controlador*/
+(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBjz3yZP8fQTWBWLmqhJkiPeU8XFKawzeM",
+    authDomain: "promos-c3fbe.firebaseapp.com",
+    databaseURL: "https://promos-c3fbe.firebaseio.com",
+    projectId: "promos-c3fbe",
+    storageBucket: "promos-c3fbe.appspot.com",
+    messagingSenderId: "258104913182"
+  };
+  firebase.initializeApp(config);
+  const auth = firebase.auth(); //Para autenticar
+  //REAL TIME
+  auth.onAuthStateChanged( firebaseUser => {
+    if(firebaseUser) {
+      console.log(firebaseUser);
+      // sessionStorage.id = firebaseUser.uid;
+      // sessionStorage.nombre = 
+    } else {
+      console.log("No logeado");
+    }
+  });
+
+angular
+    .module('registrarse-app', ['firebase'])
+    .controller('registrarse-controlador', function($scope, $firebaseObject){
+
+      $scope.validaFormulario = function(nombre, correo, pass1, pass2) {
+
+          var passError = $('#pass-err');
+          var correoError = $('#correo-existe');
+          passError.hide();
+          correoError.hide();
+          if (pass1.length < 6 | pass2 < 6) {
+            passError.text('La contraseña debe de ser por lo menos 6 caractares');
+            passError.show();
+          } else if (pass1 !== pass2) {
+            passError.text('Las contraseñas no coiciden');
+            passError.show();
+          } else {
+            $scope.guardaNuevoUsuario(correo, pass1, nombre);
+          }
+
+
+        };
+
+        $scope.guardaNuevoUsuario = function(email, pass, nombre) {
+          
+          const promise = auth.createUserWithEmailAndPassword(email, pass)
+            .then(function (user) {
+              //Exito registrando el usuario
+             var usersRef = firebase.database().ref('promos').child('usuarios');
+              usersRef.push({
+                id: user.uid,
+                correo: user.email,
+                nombre: nombre
+              });
+
+              $scope.iniciaSesion(email, pass);
+              sessionStorage.id = user.uid;
+              sessionStorage.nombre = nombre;
+              location.href = "index.html";
+            })
+            .catch( function(error) {
+              //Error
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                if (errorCode === 'auth/email-already-in-use') {
+                  $('#correo-existe').show();
+                }
+                console.log(errorCode);
+          });
+          promise.catch( e => console.log(e.message));
+
+          
+        };
+
+        $scope.iniciaSesion = function(email, pass) {
+          const promise = auth.signInWithEmailAndPassword(email, pass);
+        };
+
+    });
+}());
